e0956c7b2911650f18c4a93ec87f8bc17ce50729
Mohamad Wahyudin <mwahyudin@pm.me>
2021-05-30 17:04:10 +0700

    api responser

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function RequestSuccess($data)
+    {
+        return response()->json(['message' => 'Successfully', 'data' => $data], 200);
+        # code...
+    }
+
+    public function RequestFailed()
+    {
+        return response()->json(['message' => 'Failed !', 'data' => null], 500);
+        # code...
+    }
+
+    public function Unauthhorized()
+    {
+        return response()->json(['message' => 'You do not have permission to access thi data',], 403);
+        # code...
+    }
 }
