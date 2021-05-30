e6be60ee465cf16e887d7a1f74d5d2335556ef55
Mohamad Wahyudin <mwahyudin@pm.me>
2021-05-30 17:00:56 +0700

    routing api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AnnualLeaveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,37 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Auth::routes();
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('hi', function () {
+        return 'hi';
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::post('register', [UserController::class, 'store']);
+        Route::get('show', [UserController::class, 'show']);
+    });
+
+    Route::prefix('annual-leave')->group(function () {
+
+        //superadmin,staff hr
+        // list-application
+        Route::get('list', [AnnualLeaveController::class, 'showEmployeeLeave']);
+        Route::get('list-application', [AnnualLeaveController::class, 'showListApplicationEmployee']);
+        Route::get('list-detail', [AnnualLeaveController::class, 'showEmployeeLeaveDetail']);
+        Route::post('store', [AnnualLeaveController::class, 'AddAnnualLiveEmployee']);
+        Route::post('store-detail', [AnnualLeaveController::class, 'AddTotalAnnualLiveEmployee']);
+        Route::post('acc-application', [AnnualLeaveController::class, 'accApplication']);
+        // Route::get('show', [UserController::class, 'show']);
+
+        //employee
+        // storeleaveApplication
+        Route::get('leave-remaining', [AnnualLeaveController::class, 'showRemainingAnnualLeave']);
+        Route::post('leave-application', [AnnualLeaveController::class, 'storeLeaveApplication']);
+    });
 });
